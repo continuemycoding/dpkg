a9663d46682908fdcce9e5865304bbc1d0ab155c
test <test@gmail.com>
2026-01-25 09:00:37 +0800

    3x

--- app.ts
+++ app.ts
@@ -108,6 +108,7 @@ async function uploadToCOS(localFilePath: string, targetKey?: string): Promise<v
     const files = fs.readdirSync(debsDir);
 
     try {
+        await uploadToCOS("CydiaIcon@3x.png");
         await uploadToCOS("CydiaIcon.png");
         await uploadToCOS("Release");
 
